043ae645a1bec3d1b6aaa8b3b0aaf9931140d111
kamegawayuki <kamegawayuki@gmail.com>
2022-01-28 15:36:26 +0900

    second-commit

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,11 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
 	root 'top#index'
+
+	resources :list, only: %i(new create edit update)
   # Defines the root path route ("/")
   # root "articles#index"
+	devise_scope :user do
+    get '/users/sign_out' => 'devise/sessions#destroy'
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_01_24_065546) do
+ActiveRecord::Schema.define(version: 2022_01_27_061413) do
+
+  create_table "lists", force: :cascade do |t|
+    t.string "title", null: false
+    t.integer "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_lists_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -20,8 +28,10 @@ ActiveRecord::Schema.define(version: 2022_01_24_065546) do
     t.datetime "remember_created_at", precision: 6
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "name", default: "", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "lists", "users"
 end
