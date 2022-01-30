f9979c30be008f17d3a8e1bc9731282ee5e3ab56
kamegawayuki <kamegawayuki@gmail.com>
2022-01-30 20:45:13 +0900

    fourth-commit

--- Gemfile
+++ Gemfile
@@ -58,6 +58,7 @@ gem "bootsnap", require: false
 group :development, :test do
   # See https://guides.rubyonrails.org/debugging_rails_applications.html#debugging-with-the-debug-gem
   gem "debug", platforms: %i[ mri mingw x64_mingw ]
+	gem 'rails_admin'
 end
 
 group :development do
@@ -77,3 +78,4 @@ group :test do
   gem "selenium-webdriver"
   gem "webdrivers"
 end
+gem "sassc-rails"
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,13 @@
 Rails.application.routes.draw do
+  mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
 	root 'top#index'
 
-	resources :list, only: %i(new create edit update destroy)
+	resources :list, only: %i(new create edit update destroy) do
+		resources :card, except: %i(index)
+	end
   # Defines the root path route ("/")
   # root "articles#index"
 	devise_scope :user do
